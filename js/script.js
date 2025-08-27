cd3d98198eb00d410c385ee8a2759879160c3670
Lera <may_be_lera@inbox.ru>
2025-08-27 18:00:14 +0300

    .

--- js/script.js
+++ js/script.js
@@ -40,32 +40,7 @@ function burgerInit(e) {
             document.body.classList.remove('body--opened-modal')
         }
     }
-// ----------
-//  const cards = document.querySelectorAll('.card__item');
-//             const loadMoreBtn = document.querySelector('.card__show-more');
-//             const CARDS_PER_LOAD = 4;
-//             let visibleCount = 8;
 
-//             // Показываем кнопку если есть скрытые карточки
-//             if (cards.length > 8) {
-//                 loadMoreBtn.classList.remove('card-hidden');
-//             }
-
-//             // Обработчик кнопки
-//             loadMoreBtn.addEventListener('click', function(e) {
-//                 e.preventDefault()
-//                 // Показываем следующие 4 карточки
-//                 for (let i = visibleCount; i < visibleCount + CARDS_PER_LOAD && i < cards.length; i++) {
-//                     cards[i].classList.remove('card-hidden');
-//                 }
-                
-//                 visibleCount += CARDS_PER_LOAD;
-                
-//                 // Скрываем кнопку если больше нет карточек
-//                 if (visibleCount >= cards.length) {
-//                     loadMoreBtn.classList.add('card-hidden');
-//                 }
-//             });
 
 
 const cards = document.querySelectorAll('.card__item');
